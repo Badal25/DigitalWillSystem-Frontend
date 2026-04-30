bd14e9209712072e2afa85abc7174cd61e0bac34
Badal Wasnik <131730262+Badal25@users.noreply.github.com>
2026-04-30 19:24:44 +0530

    Update nomineeAPI.js

--- src/nominee/services/nomineeAPI.js
+++ src/nominee/services/nomineeAPI.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/nominee",
+  baseURL: "https://digitalwillsystem-backend.onrender.com/api/nominee",
 });
 
 export const verifyToken = (token) => API.get(`/verify/${token}`);
+
 export const uploadDocuments = (token, formData) =>
   API.post(`/upload/${token}`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
+
 export const fetchAssets = (token) => API.get(`/assets/${token}`);
