1899c5c07fac740fd0c7ceb4d971f6574a01a3ca
Badal Wasnik <131730262+Badal25@users.noreply.github.com>
2026-04-30 19:26:24 +0530

    Update adminAPI.js

--- src/admin/services/adminAPI.js
+++ src/admin/services/adminAPI.js
@@ -1,68 +1,7 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api/admin",
-// });
-
-// /* ================= TOKEN INTERCEPTOR ================= */
-
-// API.interceptors.request.use((req) => {
-
-//   const token = localStorage.getItem("adminToken");
-
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return req;
-// });
-
-// /* ================= ADMIN LOGIN ================= */
-
-// export const loginAdmin = (data) => API.post("/login", data);
-
-// /* ================= DASHBOARD ================= */
-
-// export const getDashboard = () => API.get("/death-overview");
-
-// /* ================= DEATH LIST ================= */
-
-// export const getDeathList = () => API.get("/death-list");
-
-// /* ================= ACTIONS ================= */
-
-// export const verifyDocuments = (id) => API.post(`/verify/${id}`);
-
-// export const approveCase = (id) => API.post(`/approve/${id}`);
-
-// export const rejectCase = (id) => API.post(`/reject/${id}`);
-
-// export const releaseCase = (id) => API.post(`/release/${id}`);
-
-// /* ================= INACTIVE USERS ================= */
-
-// export const getInactiveUsers = () => API.get("/inactive-users");
-
-// /* ================= NOMINEE ================= */
-
-// export const notifyNominee = (id) =>
-//   API.post(`/notify-nominee/${id}`);
-
-// export const getNomineeAccess = () =>
-//   API.get("/nominee-access");
-
-// /* ================= AUDIT ================= */
-
-// export const getAuditLogs = () =>
-//   API.get("/audit-logs");
-
-// export default API;
-
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/admin",
+  baseURL: "https://digitalwillsystem-backend.onrender.com/api/admin",
 });
 
 // 🔥 TOKEN INTERCEPTOR FIXED
