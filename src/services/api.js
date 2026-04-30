a7b25b09a4dcbfdafe9c725f2e6580f3bff02800
Badal Wasnik <131730262+Badal25@users.noreply.github.com>
2026-04-30 19:22:15 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,10 @@
-
-
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api", // backend base url
-// });
-
-// // Automatically add Authorization header
-// API.interceptors.request.use((config) => {
-//   // Correct token key
-//   // const token = localStorage.getItem("adminToken"); 
-//   const token = localStorage.getItem("token"); // For user side
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default API;
-
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://digitalwillsystem-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   console.log("TOKEN:", token);
